9091e5deba2f5ed9c7c38c07e7286c1dbc98e67d
TessaRidderikhoff <tessaridderikhoff@gmail.com>
2018-05-04 12:30:01 +0200

    alles commit (van Tessa)

---
+++ schedule.py
@@ -0,0 +1,9 @@
+import lectures
+
+roomlocks = list(range(0, 140))
+# print(roomlocks)
+schedule = dict.fromkeys(roomlocks)
+print(allcourses[1])
+schedule[0] = allcourses[1].courseName
+print(schedule)
+
