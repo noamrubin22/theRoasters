518ad52bf3cc3df5746e6a57b9737ab02541cb7c
noamrubin22 <noamru22@gmail.com>
2018-05-15 13:59:26 +0200

    added comments to scorefunction

--- code/parse.py
+++ code/parse.py
@@ -3,29 +3,31 @@ from classes import Students
 
 MY_FILE = "../data/studenten_roostering.csv"
 
+# create empty list for students
 student_list = []
+
 def parse(raw_file, delimiter):
 
-    # Open CSV file, and safely close it when we're done
+    # open CSV file
     opened_file = open(raw_file)
 
+    # read and clean CSV data
     csv_data = csv.reader((x.replace('\0', '') for x in opened_file), delimiter=delimiter)
 
     # Read the CSV data
 #    csv_data = csv.reader(opened_file, delimiter=delimiter)
 
-    # Setup an empty list
+    # setup an empty list
     parsed_data = []
 
-    # Skip over the first line of the file for the headers
+    # skip over the first line of the file for the headers
     fields = next(csv_data)
 
-    # Iterate over each row of the csv file, zip together field -> value
+    # iterate over each row of the csv file, zip together field -> value
     for row in csv_data:
         parsed_data.append(dict(zip(fields, row)))
 
-
-    # Close the CSV file
+    # close the CSV file
     opened_file.close()
 
     return parsed_data
@@ -34,10 +36,11 @@ def gimme_students():
     return student_list;
 
 def createStudentClass():
-    # Call our parse function and give it the needed parameters
+
+    # call our parse function and give it the needed parameters
     new_data = parse(MY_FILE, ";")
 
-
+    # iterate over new data
     for i in range(len(new_data)):
 
         # extract all information
@@ -46,9 +49,8 @@ def createStudentClass():
         student_id = new_data[i]['studentID']
         courses = []
 
-
         # dict1_values = {k*2:v for (k,v) in dict1.items()}
-        # Add extract all courses from all students
+        # extract all courses from all students and add to list
         if new_data[i]['course_1'] is not '':
             courses.append(new_data[i]['course_1'])
         if new_data[i]['course_2'] is not '':
@@ -60,6 +62,7 @@ def createStudentClass():
         if new_data[i]['course_5'] is not '':
             courses.append(new_data[i]['course_5'])
 
+        # append students to list with extracted info
         student_list.append(Students(last_name, first_name, student_id, courses))
 
     return student_list
--- code/scorefunction.py
+++ code/scorefunction.py
@@ -2,64 +2,136 @@ import main
 from main import translateRoomlock
 
 def scorefunction(allcourses, student_list, chambers):
+	""" this function calculates the score for the created schedule"""
 
-	# valid schedule has been made: 1000 points
+	# start-score of a valid schedule
 	points = 1000;
+	roomlocks_per_day = 28;
 
-	# subtract or add points based on schedule
-	# loop through courses
+	#* create groups and dict to keep up the days per activity *#
+
+	# iterate over courses
 	for course in allcourses:
+
+		# if seminar(s) exist
 		if course.seminars > 0:
+
+			# create list with amount of seminar-groups
 			groups = list(range(1, course.seminars + 1))
+
+		# if practical exist
 		elif course.practicals > 0:
+
+			# create list with amount of practical-groups
 			groups = list(range(1, course.practicals + 1))
+
+		# otherwise only one group (lecture)
 		else:
 			groups = [1]
 
+		# create dict with groupnumber as key, [days] as values
 		dayActivity = {k: [] for k in groups}
 
-		# loop trough activities of a course
+
+		#* subtract points based on activities per day *#
+		
+		# iterate over activities per course
 		for activity in course.activities:
-			day = int(activity[0]/28)
+
+			# determine day
+			day = int(activity[0] / roomlocks_per_day)
+
+			# if activity = lecture 
 			if activity[2] == 0:
+
+				# substract points when on the same day
 				if day in dayActivity[1]:
 					points -= 10	
+
+				# add day to list linked to lecture
 				for group in groups:	
 					dayActivity[group].append(day)
-			else:
+
+			# for seminars 
+			elif activity[2] == 1:
+
+				# substract points when on the same day
 				if day in dayActivity[activity[2]]:
 					points -= 10
+
+				# add day to list linked to group
+				dayActivity[activity[2]].append(day) 
+
+			# for practicals 
+			else:
+
+				# substract points when on the same day
+				if day in dayActivity[activity[2]]:
+					points -= 10
+
+				# add day to list linked to group
 				dayActivity[activity[2]].append(day) 
 		# print(course.name, dayActivity)
 
+
+		#* add points for spreading of activities over week *#
+
+		# iterate over groups
 		for group in groups:
+
+			# when 2 activities in week
 			if (len(dayActivity[group]) == 2):
+
+				# add points if they are spread (mo- thu, mo-fri)
 				if (abs(dayActivity[group][0] - dayActivity[group][1]) >= 3):
 					points += 20
-			# if three activities in the week: mo-we-fr?
+
+			# when 3 activities in the week
 			if (len(dayActivity[group]) == 3):
+
+				# add points if spread (mo-we-fr)
 				if 0 in dayActivity[group] and 2 in dayActivity[group] and 4 in dayActivity[group]:
 					points += 20
-			# if four activities in the week: mo-tu-th-fr?
+
+			# if 4 activities in the week
 			if (len(dayActivity[group]) == 4):
+
+				# add points if spread  (mo-tu-th-fr)
 				if 0 in dayActivity[group] and 1 in dayActivity[group] and 3 in dayActivity[group] and 4 in dayActivity[group]:
 					points += 20
 
 
-		
 	print(student_list[0].schedule)
 
+	# for all students
 	for student in student_list:
+
+		# create empty list
 		timelocksStudent = []
+
+		# for each activity in schedule
 		for activity in student.schedule:
+
+			# if roomlock is occupied substract points
 			if activity[0] in timelocksStudent:
 				points -= 1
+
+			# add roomlock to list
 			timelocksStudent.append(activity[0])
 
+	# for all courses
 	for course in allcourses:
+
+		# and activities
 		for activity in course.activities:
+
+			# substract room and timelock
 			room, timelock = translateRoomlock(activity[0])
+
+			# for lectures
 			if activity[2] == 0:
+
+				# and too many students for room, substract points
 				if int(chambers[room].capacity) < course.students:
 					print(chambers[room].capacity)
 					print(course.students)
@@ -67,16 +139,37 @@ def scorefunction(allcourses, student_list, chambers):
 					maluspoints = course.students - int(chambers[room].capacity)
 					print(maluspoints)
 					points -= maluspoints
-			else:
-				if course.seminars > 0:
-					if int(chambers[room].capacity) < course.maxstudentssem:
-						print(chambers[room].capacity)
-						print(course.maxstudentssem)
-						maluspoints = course.maxstudentssem - int(chambers[room].capacity)
-						print(maluspoints)
-						points -= maluspoints
 
-	# laat eindscore zien (so far)
+			# for seminars
+			elif activity[2] == 1:
+
+				# # if seminar
+				# if course.seminars > 0:
+
+				# and too many students fro room, substract points
+				if int(chambers[room].capacity) < course.maxstudentssem:
+					print(chambers[room].capacity)
+					print(course.maxstudentssem)
+					maluspoints = course.maxstudentssem - int(chambers[room].capacity)
+					print(maluspoints)
+					points -= maluspoints
+
+			# if practical
+			else:
+				# # if seminar
+				# if course.seminars > 0:
+
+				# and too many students fro room, substract points
+				if int(chambers[room].capacity) < course.maxstudentsprac:
+					print(chambers[room].capacity)
+					print(course.maxstudentsprac)
+					maluspoints = course.maxstudentsprac - int(chambers[room].capacity)
+					print(maluspoints)
+					points -= maluspoints
+			#
+
+
+	# show points
 	print("Points: ", points)
 	return points
 
