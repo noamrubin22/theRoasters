3c354bb2462376bb25f67912d3513d08f6cd8994
Najib <nelmibit@gmail.com>
2018-05-23 13:13:57 +0200

    commenting

--- code/genetic.py
+++ code/genetic.py
@@ -10,7 +10,6 @@ import random
 def initial_population(amount):
 
     population = []
-    parents_max = 10
     scores = []
 
     for i in range(amount):
@@ -20,66 +19,81 @@ def initial_population(amount):
         # create a new random schedule
         chambers, allcourses, student_list, schedule = createSchedule()
 
-        # add all information about this specific schedule to an array
+        # add all information about this specific schedule
         score_info.append(allcourses)
         score_info.append(student_list)
         score_info.append(chambers)
 
-
+        # add individual schedule-info to timetable array
         timetable_info.append(score_info)
         timetable_info.append(schedule)
 
-        # add the array of information about this schedule to the timetable_info array
+        # add the array with individual timetable-info to the population
         population.append(timetable_info)
 
     return population
 
 
 def selection(population):
+    """ Calculates the fitness of an individual by returning the schedule score """
 
     mating_pool = []
     scores = []
 
-    def points_score(timetable_info):
+    def fitness(timetable_info):
+        """ Calculates the fitness of an individual """
+
         return calcScore(timetable_info[0][0],
                          timetable_info[0][1],
                          timetable_info[0][2])
 
-    population = sorted(population, key=points_score, reverse=True)
+    # choose the fittest individuals
+    population = sorted(population, key=fitness, reverse=True)
 
-    multiply = 10
+    # set max and range
+    probability = 10
     parents_max = 10
 
+    # create mating pool, with the fittest schedules
     for i in range(parents_max):
-        for j in range(multiply):
+        for j in range(probability):
+
+            # fittest schedules have highest probability
             scores.append(calcScore(population[i][0][0], population[i][0][1], population[i][0][2]))
             mating_pool.append(population[i])
-        multiply -= 1
+
+        # decrease probability
+        probability -= 1
 
     print(scores)
 
     return mating_pool
 
 
-def cross_over(mating_pool):
+def cross_over(mating_pool, offspring):
+    """ Creates offspring from mating pool by exchanging genes """
 
+    # create empty list for children
     children = []
 
-    for i in range(new_population):
+    for i in range(offspring):
 
         # first create an empty schedule
         schedule = createEmptySchedule()
-        
+
+        print(schedule)
+
+        # get a mother and father from the mating pool
         mother = mating_pool[random.randint(0, len(mating_pool))]
         father = mating_pool[random.randint(0, len(mating_pool))]
 
-    print(mother, father)
+
+
+
 
 
 
 mating_pool = selection(initial_population(100))
-
-cross_over(mating_pool)
 
 
 #     for i in range(new_population):
