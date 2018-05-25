e5144c4e3a23b3d0c6f440cefc835883126edbdf
Najib <nelmibit@gmail.com>
2018-05-25 20:31:30 +0200

    pushing to merge

--- code/algorithms.py
+++ code/algorithms.py
@@ -1,28 +1,28 @@
-import helpers 
+import helpers
 
 def hillclimbStudent(times, chambers, allcourses, student_list, schedule):
 	""" Performs student swap and changes back if it does not lead to a better score """
-	
+
 	# perform swap a given number of times
 	for i in range(0, times):
-		
+
 		# calculate score before swap
 		points = calcScore(allcourses, student_list, chambers)
 
 		# perform swap (and save what has been swapped)
 		swapcourse, sem1, sem2, prac1, prac2, student1, student2 = swapStudents(chambers, allcourses, student_list, schedule)
-		
+
 		# calculate score after swap
 		newpoints = calcScore(allcourses, student_list, chambers)
-		
+
 		# swap back if new score is not better
 		if newpoints < points:
-			
+
 			swapStudents(chambers, allcourses, student_list, schedule, swapcourse, sem2, sem1, prac2, prac1, student2, student1)
-			
+
 			# calculate score after swap back
 			newpoints = calcScore(allcourses, student_list, chambers)
-			
+
 			# stop algorithm if swapping back does not lead to original score
 			if newpoints != points:
 				break
@@ -78,7 +78,7 @@ def simulatedAnnealing(coolingscheme, min_iterations, chambers, allcourses, stud
 	# array for scores (visualization)
 	scores = []
 
-	# set start temperature 
+	# set start temperature
 	temperature = temp_start
 
 	# loop until system has cooled
@@ -91,19 +91,19 @@ def simulatedAnnealing(coolingscheme, min_iterations, chambers, allcourses, stud
 		scores.append(points)
 
 		# keep track of the best score and its variables
-		if best_score == None or points > best_score: 
+		if best_score == None or points > best_score:
 			best_score = points
 			best_courses = allcourses
 			best_student_list = student_list
 			best_chambers = chambers
 
-		# pick random neighbour by swapping 
+		# pick random neighbour by swapping
 		course1, activity1, course2, activity2, schedule = swapCourse(chambers, allcourses, student_list, schedule)
 
 		# calculate new score
 		newpoints = calcScore(allcourses, student_list, chambers)
-		
-		# if new score is worst 
+
+		# if new score is worst
 		if newpoints < points:
 
 			# calculate acceptance chance
@@ -111,7 +111,7 @@ def simulatedAnnealing(coolingscheme, min_iterations, chambers, allcourses, stud
 
 			# if acceptance chance higher than random number
 			if (acceptance_probability > random.random()):
-			
+
 				# accept the worst solution
 				points = newpoints
 
@@ -127,7 +127,7 @@ def simulatedAnnealing(coolingscheme, min_iterations, chambers, allcourses, stud
 				# calculate new score and print
 				newpoints = calcScore(allcourses, student_list, chambers)
 
-				# if back-swap didn't go well 
+				# if back-swap didn't go well
 				if points != newpoints:
 
 					# print coursenames and break loop
@@ -139,20 +139,20 @@ def simulatedAnnealing(coolingscheme, min_iterations, chambers, allcourses, stud
 				points = newpoints
 
 		# if new score is better
-		else: 
+		else:
 
 			# accept it
-			points = newpoints 
+			points = newpoints
 			# print("accepted score:" )
 
 
 	print("bestscore:", best_score)
 
-
 	return best_score, best_courses, best_student_list, best_chambers, scores
 
+
 def genetic(initial, survival_rate, offspring, generations, mutation):
-    """ Implements a genetic algorithm on the scheduling problem """
+    """ Implements a genetic algorithm on scheduling problem """
 
     # creates initial population
     print("Creating initial population...")
@@ -162,9 +162,9 @@ def genetic(initial, survival_rate, offspring, generations, mutation):
     print("Selecting fittest individuals...")
     fittest = selection(genesis, survival_rate)
 
-    # performs cross over
-    print("Mating :)))))))")
+    # apply crossover to the fittest schedules
     children = cross_over(fittest, offspring, 0, mutation)
+    print("\t\tSomething about the birds and the bees...\n")
 
     # for amount of generations
     for i in range(generations):
@@ -174,7 +174,6 @@ def genetic(initial, survival_rate, offspring, generations, mutation):
 
         # perform cross over, add mutation
         children = cross_over(fittest, offspring, i + 1, mutation)
-        # print("fittest: ", calc_score(fittest[0][0][0], fittest[0][0][1], fittest[0][0][2]))
 
     # select fittest children
     fittest = selection(children, survival_rate)
@@ -185,15 +184,9 @@ def genetic(initial, survival_rate, offspring, generations, mutation):
     student_list = fittest[0][0][1]
     schedule = fittest[0][1]
 
-    # calculate score 
+    # calculate score
     fittest_score = calc_score(allcourses, student_list, chambers)
     print("fittest: ", fittest_score)
     print("schedule: ", schedule)
-    print("Start hillclimbing on fittest schedule...")
 
-    # write scores in txt file
-    score_file = open('scores.txt', 'w')
-    score_file.write(str(total_gen_scores))
-
-    # swap between roomlocks using a hillclimber
-    hillclimb_roomlocks2(1000, chambers, allcourses, student_list, schedule)
+    return schedule, allcourses, student_list, chambers
