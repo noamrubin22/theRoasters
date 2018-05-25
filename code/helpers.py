d1d312f9ab13c35318668541b82a8bd7aeae8dc0
Najib <nelmibit@gmail.com>
2018-05-25 20:49:38 +0200

    moved some functions to helpers.py

--- code/helpers.py
+++ code/helpers.py
@@ -286,7 +286,7 @@ def create_schedule():
 	return chambers, allcourses, student_list, schedule
 
 def update_classes_from_schedule(schedule):
-	""" Updates classes from new schedule """ 
+	""" Updates classes from new schedule """
 
 	# load all student, courses and room -information into variables
 	allcourses = create_courses()
@@ -305,7 +305,7 @@ def update_classes_from_schedule(schedule):
 			# if lecture
 			if "lecture" in activity:
 
-				# split text 
+				# split text
 				splittext = activity.split(" lecture ")
 
 				# assign class
@@ -860,6 +860,18 @@ def gem_exp(min_iterations, i):
         return sigmoidal(min_iterations, i)
 
 
+# import main
+import csv
+import re
+# from main import create_schedule
+from scorefunction import calc_score
+from generateschedule import create_empty_schedule, create_schedule
+import random
+from hillclimber import swapCourse, hillclimb_roomlocks
+from generateschedule import updateClassesFromSchedule
+from hillclimberscheduleplaces import swapCourse2, hillclimb_roomlocks2
+
+
 def initial_population(amount):
     """ Creates an intial population and returns the parents """
 
@@ -868,16 +880,11 @@ def initial_population(amount):
 
     for i in range(amount):
 
-        # print("Generating initial population...")
-
         timetable_info = []
         score_info = []
 
         # create a new random schedule
         chambers, allcourses, student_list, schedule = create_schedule()
-
-        # print("Hillclimbing on schedule {}...".format(i))
-        # hillclimb_roomlocks2(20, chambers, allcourses, student_list, schedule)
 
         # add all information about this specific schedule
         score_info.append(allcourses)
@@ -904,8 +911,6 @@ def selection(population, rate):
     def fitness(timetable_info):
         """ Calculates the fitness of an individual """
 
-        # print("Calculating fitness scores...")
-
         return calc_score(timetable_info[0][0],
                          timetable_info[0][1],
                          timetable_info[0][2])
@@ -922,19 +927,6 @@ def selection(population, rate):
         # fittest schedules have highest probabilities
         scores.append(calc_score(population[i][0][0], population[i][0][1], population[i][0][2]))
         mating_pool.append(population[i])
-
-    # iterate over parents
-    # for i in range(rate):
-    #
-    #     # create matingpool
-    #     for j in range(probability):
-    #
-    #         # fittest schedules have highest probabilities
-    #         scores.append(calc_score(population[i][0][0], population[i][0][1], population[i][0][2]))
-    #         mating_pool.append(population[i])
-    #
-    #     # decrease probability
-    #     probability -= 1
 
     return mating_pool
 
@@ -962,7 +954,6 @@ def cross_over(mating_pool, offspring, generation, chance):
     children = []
     fittest_score = 0
     chance_array = []
-    gen_scores = []
 
     # determine probability
     probability = len(mating_pool)
@@ -978,9 +969,6 @@ def cross_over(mating_pool, offspring, generation, chance):
 
         # decrease probability
         probability -= 1
-
-    # set fittest_score
-    fittest_score = 0
 
     # iterate over offspring
     for i in range(offspring):
@@ -999,13 +987,6 @@ def cross_over(mating_pool, offspring, generation, chance):
 
         # until no activities are left
         while activities > 0:
-
-            # print("Applying crossover between parents...")
-
-            # if activities % 2 == 0:
-            #     parent_schedule = parents[0][1]  # >>> [[allcourses, chambers, studentlist], schedule]
-            # else:
-            #     parent_schedule = parents[1][1]
 
             # choose random course from parent schedule
             random_course = random.randint(0, len(parent_schedule) - 1)
@@ -1047,7 +1028,7 @@ def cross_over(mating_pool, offspring, generation, chance):
                     # increase parent-counter
                     newparentcounter += 1
 
-                    # if 500 new parents weren"t enough
+                    # if 500 new parents weren't enough
                     if newparentcounter > 500:
                         break
 
@@ -1079,22 +1060,18 @@ def cross_over(mating_pool, offspring, generation, chance):
         # calculate score
         score = calc_score(allcourses, student_list, chambers)
 
-        # add to score file
-        gen_scores.append(score)
-
         # if score is better than the fittest
         if score > fittest_score:
 
             # adjust fittest score
             fittest_score = score
 
+            # print best new score of the generation to the console
             print("New best found ---> Schedule: {}, generation: {}, score: {}".format(i, generation, score))
 
         # add the array with individual timetable-info to the population
         children.append(timetable_info)
 
-    # create score dict
-    total_gen_scores[generation] = gen_scores
 
     return children
 
