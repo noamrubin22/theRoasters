09b28e70c1aed140512636c30a9b0121ed5e154c
Najib <32820051+Najibit@users.noreply.github.com>
2018-05-28 20:55:26 +0200

    Create sdfsdf.py

---
+++ sdfsdf.py
@@ -0,0 +1,9 @@
+# xas = []
+# for i in range(201):
+#     xas.append(i)
+# print(len(highs))
+# plt.plot(xas, means, linewidth=2.0, animated=True)
+# plt.plot(xas, lows, animated=True)
+# plt.plot(xas, highs, animated=True)
+# plt.axis([0, 200, 0, 1800])
+# plt.show()
